e6110c165b52467e1d9c28f7fdff2748c7b6eac3
Manuel Zuniga <zuniga@ualberta.ca>
2025-07-23 22:09:49 -0600

    add other routes for testing

--- src/index.mjs
+++ src/index.mjs
@@ -5,7 +5,15 @@ const app = express()
 const port = 2020
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.status(200).send('Hello World!')
+})
+
+app.get('/our-story', (req, res) => {
+  res.status(200).send('Our Story')
+})
+
+app.get('/our-programs', (req, res) => {
+  res.status(200).send('Our Programs')
 })
 
 app.listen(port, () => {
